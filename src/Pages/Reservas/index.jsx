ea16a35d0b04848a0691b45daffe02135253b28d
Breno Nascimento <satsukebare23@gmail.com>
2022-06-25 23:17:23 -0300

    estilizando componente de reservas

--- src/Pages/Reservas/index.jsx
+++ src/Pages/Reservas/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import './style.css'
 
 class Reservas extends React.Component {
   render() {
     return (
       <div>
-        <h1>reservas!</h1>
+        <h1 className='title'>Você solicitou 1 reservas!</h1>
+
+        <div className='reservas'>
+          <img
+            src='https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg'
+            alt='maceio'
+            />
+            <strong>Viagem maceio 7 idas</strong>
+            <span>quantidade 2</span>
+            <button
+              type='button'
+              onClick={() => {}}>
+              <span>
+                deletar
+              </span>
+            </button>
+        </div>
       </div>
     )
   }
