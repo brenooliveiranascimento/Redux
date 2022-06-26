658ab86e05b367fe4d08a63a1a241ff6b29fbefa
Breno Nascimento <satsukebare23@gmail.com>
2022-06-25 22:26:00 -0300

    first commit

---
+++ src/Routes/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+class Router extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route/>
+      </Switch>
+    )
+  }
+}
+
+export default Router
